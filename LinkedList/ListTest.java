535d3c3bb742b11ecdd5f9f920a0e4994523355e
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-12-07 18:59:05 +0200

    Create ListTest.java

---
+++ LinkedList/ListTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ * Simple test for {@link List}
+ *
+ */
+
+public class ListTest
+{
+	public static void main( String args[] )
+	{
+		List list = new List(); // create the List container
+
+		// insert integers in list
+		list.insertAtFront( 5 );
+		list.print();
+		list.insertAtFront( 4 );
+		list.print();
+		list.insertAtBack( 1 );
+		list.print();
+		list.insertAtBack( 6 );
+		list.print();
+		list.insertAtBack(3 );
+		list.print();
+
+		// remove objects from list; print after each removal
+		try
+		{
+			Object removedObject = list.removeFromFront();
+			System.out.printf( "%s removed\n", removedObject );
+			list.print();
+
+			removedObject = list.removeFromFront();
+			System.out.printf( "%s removed\n", removedObject );
+			list.print();
+
+			removedObject = list.removeFromBack();
+			System.out.printf( "%s removed\n", removedObject );
+			list.print();
+
+			removedObject = list.removeFromBack();
+			System.out.printf( "%s removed\n", removedObject );
+			list.print();
+
+			removedObject = list.removeFromBack();
+			System.out.printf( "%s removed\n", removedObject );
+			list.print();
+		} // end try
+		catch ( EmptyListException emptyListException )
+		{
+			emptyListException.printStackTrace();
+		} // end catch
+
+	} // end main
+} // end class ListTest
+
+
+
