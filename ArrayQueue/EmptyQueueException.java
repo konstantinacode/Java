08898c0efba744940bafc85b2fcd90c52341a7fc
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-12-21 20:10:48 +0000

    Array Queue

---
+++ ArrayQueue/EmptyQueueException.java
@@ -0,0 +1,17 @@
+/**
+ * Exception class for accessing emtpy queues
+ *
+ */
+public class EmptyQueueException extends RuntimeException {
+	public EmptyQueueException(String mesg) {
+		super(mesg);
+	}
+	
+	public EmptyQueueException(Throwable t) {
+		super(t);
+	}
+	
+	public EmptyQueueException(String mesg, Throwable t) {
+		super(mesg, t);
+	}
+}
