fc607f1474a97b415e2f02deec4052ef927b1379
Konstantina Matthaiou <konstantina_mat@hotmail.com>
2018-12-29 22:08:07 +0200

    Update ListSortExample.java

--- ListSort/ListSortExample.java
+++ ListSort/ListSortExample.java
@@ -1,7 +1,7 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class ListSortExample{ 
-    	
+    
     static class Node{ 
         int val; 
         Node next;
@@ -12,30 +12,34 @@ public class ListSortExample{
     }
 
     static Node create(){ 
-		Node head = null;
-	
-		Scanner input = new Scanner(System.in);
-		while (input.hasNextInt()){
-			int in = input.nextInt();
-			if (head == null)
-				head = new Node (in, null);
-			else
-				head = new Node (in, head);
-		}
-		
-		in.close();
-		
-		return head;
+        Node head = new Node(0, null);
+        Scanner input = new Scanner(System.in);
+		//input example: 1 4 2 9 5 3 0 11 q
+        while (input.hasNextInt()){
+            Node node = new Node(input.nextInt(), null);
+            node.next = head.next;
+            head.next = node;
+            //or head.next = new Node(input.nextInt(), head.next);
+        }
+		input.close();
+        return head;
     }
     
     static Node sort(Node head){ 
-		
-
+        Node t, u, x, b = new Node(0, null);
+        while (head.next != null){
+            t = head.next; u = t.next; head.next = u;
+            for (x = b; x.next != null; x = x.next)
+              if (x.next.val > t.val) break;
+            t.next = x.next; x.next = t;
+        }
+        return b;
     }
     
     static void print(Node h){ 
-
-
+        for (Node t = h.next; t != null; t = t.next)
+          System.out.print(t.val + " "); 
+        System.out.println();
     }
     
     public static void main(String[] args){ 
